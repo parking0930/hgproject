f1dd9015fb45fd263ce7bb8aae48fb791ec1a9f0
parking0930 <parking0930@naver.com>
2020-09-26 12:30:34 +0900

    3주차

--- index.js
+++ index.js
@@ -55,3 +55,215 @@ app.get('/', function(req, res) {
         });
     }
 });
+
+app.get('/join',function(req,res){
+        res.render('join',{});
+});
+
+app.get('/joinNext',function(req,res){
+        res.render('joinNext',{});
+});
+
+app.post('/joinComplete',function(req,res){
+    var id = req.body.u_id;
+    var pw = req.body.u_pw;
+    var nick = req.body.u_nick;
+
+    usersql.register(nick,id,pw,function(err){
+        if(err) throw err;
+    })
+    res.redirect('/');
+});
+
+app.get('/checkNick/:nick',function(req,res){
+    var nick = req.params.nick;
+    usersql.FindNick(nick,function(err,result){
+        if(err)throw err;
+        var i = 0;
+        for(var key in result){
+            ++i;
+        }
+        if(i!=0)
+        {
+            res.send(result[0]['name']);
+        }
+        else
+        {
+            res.send(null);
+        }
+    })
+})
+
+app.get('/checkId/:id',function(req,res){
+    var id = req.params.id;
+    usersql.FindId(id,function(err,result){
+        if(err)throw err;
+        var i = 0;
+        for(var key in result){
+            ++i;
+        }
+        if(i!=0)
+        {
+            res.send(result[0]['id']);
+        }
+        else
+        {
+            res.send(null);
+        }
+    })
+})
+
+app.post('/login',function(req,res){
+    var id = req.body.u_id;
+    var pw = req.body.u_pw;
+    usersql.login(id,function(err,result){
+        if(err) throw err;
+        var i = 0;
+        for(var key in result){
+            i++;
+        }
+        if(i==0)
+        {
+            console.log("로그인 실패...");
+            res.redirect('/');
+        }
+        else
+        {
+            if(id==result[0]['id'] && pw==result[0]['pw'])
+            {
+                console.log(result[0]['name']+"님이 로그인하셨습니다.");
+                req.session.user = result[0]['name'];
+                res.redirect('/');
+            }
+            else
+            {
+                console.log("로그인 실패...");
+                res.redirect('/');
+            }
+        }
+    });
+});
+app.get('/logout',function(req,res){
+    req.session.destroy();
+    res.clearCookie('sid');
+    res.redirect('/');
+});
+app.get('/write/:board',function(req,res){
+    var board = req.params.board;
+    if(req.session.user)
+    {
+        if(board=="board_free" || board=="board_bt" || board=="board_notice")
+        {
+            if(board=="board_notice" || board=="board_event")
+            {
+                if(req.session.user == "관리자")
+                {
+                    res.render('write',{id:board,session:req.session});
+                }
+                else
+                {
+                    res.redirect('/');
+                }
+            }
+            else
+            {
+                res.render('write',{id:board,session:req.session});
+            }
+        }
+        else
+        {
+            res.redirect('/');
+        }
+    }
+    else
+    {
+        res.redirect('/');
+    }
+});
+
+app.get('/board_free',function(req,res){
+    usersql.boardCheck("board_free",function(err,result){
+        var i = 0;
+        for(var i in result)
+        {
+            i++;
+        }
+        if(i!=0)
+        {
+            res.render('freeboard',{session:req.session,id:"board_free",board:result});
+        }
+        else
+        {
+            res.render('freeboard',{session:req.session,id:"board_free",board:null});
+        }
+    });
+})
+
+app.get('/board_bt',function(req,res){
+    usersql.boardCheck("board_bt",function(err,result){
+        var i = 0;
+        for(var i in result)
+        {
+            i++;
+        }
+        if(i!=0)
+        {
+            res.render('btboard',{session:req.session,id:"board_bt",board:result});
+        }
+        else
+        {
+            res.render('btboard',{session:req.session,id:"board_bt",board:null});
+        }
+    });
+})
+
+app.get('/board_notice',function(req,res){
+    usersql.boardCheck("board_notice",function(err,result){
+        var i = 0;
+        for(var i in result)
+        {
+            i++;
+        }
+        if(i!=0)
+        {
+            res.render('noticeboard',{session:req.session,id:"board_notice",board:result});
+        }
+        else
+        {
+            res.render('noticeboard',{session:req.session,id:"board_notice",board:null});
+        }
+    });
+})
+
+app.get('/show/:board/:id',function(req,res){
+    var id = req.params.id;
+    var board = req.params.board;
+    usersql.upBoard(board,id,function(err,result){
+        if(err)throw err;
+    });
+    usersql.showWrite(board,id,function(err,result){
+        if(err)throw err;
+        var i = 0;
+        for(var i in result)
+        {
+            i++;
+        }
+        if(i!=0)
+        {
+            res.render('show',{result:result,board:board,session:req.session});
+        }
+        else
+        {
+            res.redirect('/');
+        }
+    });
+})
+app.post('/SendToMe',function(req,res){
+    var id = req.body.id;
+    var title = req.body.title;
+    var contents = req.body.contents;
+    var name = req.body.name;
+    usersql.boardWrite(id,title,contents,name,function(err,result){
+        if(err)throw err;
+    });
+});
--- usersql.js
+++ usersql.js
@@ -34,3 +34,72 @@ exports.getfiveboard = function(board,callback){
         callback(null,result);
     });
 };
+
+exports.FindNick = function(Nick,callback){
+    var q = 'SELECT name from users where name="'+Nick+'"';
+    conn.query(q,function(err,result,field){
+        if(err){callback(true,null);}
+        callback(null,result);
+    });
+};
+
+exports.FindId = function(Id,callback){
+    var q = 'SELECT id from users where id="'+Id+'"';
+    conn.query(q,function(err,result,field){
+        if(err){callback(true,null);}
+        callback(null,result);
+    });
+};
+
+exports.register = function(name,id,pw,callback){
+    conn.query('insert into users (name, id, pw) values ("' + name + '","' + id + '","' + pw + '")', function(err, rows) {
+        if(err) { callback(true);}
+        console.log(name+"님 회원가입 완료!");
+    });
+};
+
+exports.login = function(Id,callback){
+    var q = 'SELECT name,id,pw from users where id="'+Id+'"';
+    conn.query(q,function(err,result,field){
+        if(err){callback(true,null);}
+        callback(null,result);
+    });
+};
+
+exports.boardWrite = function(id,title,contents,name,callback){
+    var q = 'insert into '+id+' (title, contents, name) values ("' + title + '","' + contents + '","' + name + '")';
+    conn.query(q,function(err,result,field){
+        if(err){callback(true);}
+    });
+};
+
+exports.boardCheck = function(id,callback){
+    var q = 'SELECT id,title,name,time,ck FROM '+id+' ORDER BY id DESC';
+    conn.query(q,function(err,result,field){
+        if(err){callback(true,null);}
+        callback(null,result);
+    });
+};
+
+exports.showWrite = function(board,id,callback){
+    var q = 'SELECT title,contents,name,time,ck FROM '+board+' WHERE id='+id;
+    conn.query(q,function(err,result,field){
+        if(err){callback(true);}
+        callback(null,result);
+    });
+};
+
+exports.upBoard = function(board,id,callback){
+    var q = 'UPDATE '+board+' SET ck=ck+1 WHERE id='+id;
+    conn.query(q,function(err,result,field){
+        if(err){callback(true);}
+    });
+};
+
+exports.getfiveboard = function(board,callback){
+    var q = 'SELECT id,title FROM '+board+' ORDER BY id DESC LIMIT 6';
+    conn.query(q,function(err,result,field){
+        if(err){callback(true);}
+        callback(null,result);
+    });
+};
